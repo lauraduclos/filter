877194a9ebf390963f81238325e951e44296bbde
Laura Duclos <36208720+lauraduclos@users.noreply.github.com>
2018-04-24 15:54:32 -0400

    Add files via upload

---
+++ filter/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	$("#filter").on("keyup", function () {
+		var value = $(this).val().toLowerCase();
+		$("#content tr").filter(function () {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
